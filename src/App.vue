6c33d11f0c98c747f95eb0e6fe777220616d7fc9
Serhii Chernenko <serhii.chernenko@vaimo.com>
2024-09-07 15:48:37 +0200

    repo prepared

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { useCycleList } from '@/composables/useCycleList'
+
+// @ts-expect-error Expected 0 arguments, but got 1
+const { state, next, prev } = useCycleList([
+    'Dog',
+    'Cat',
+    'Lizard',
+    'Shark',
+    'Whale',
+    'Dolphin',
+    'Octopus',
+    'Seal',
+])
+</script>
+
+<template>
+    <div class="page">
+        <div>
+            <h1>{{ state }}</h1>
+            <div class="btn-wrapper">
+                <button @click="prev">Previous</button>
+                <button @click="next">Next</button>
+            </div>
+        </div>
+    </div>
+</template>
